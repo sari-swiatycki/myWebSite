8a2211422e4efa31719a9b04acfb2267ff5b015d
sari-swiatycki <s0534160286@gmail.com>
2025-05-27 02:39:56 +0300

    home

--- AngularSingleZone/src/components/home/home.component.ts
+++ AngularSingleZone/src/components/home/home.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatIconModule } from '@angular/material/icon';
+// import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [MatButtonModule, MatIconModule,RouterOutlet,MatCardModule, MatButtonModule,RouterLinkActive, RouterLink],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.css'
+// })
+// export class HomeComponent {
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Component } from "@angular/core"
+import { MatButtonModule } from "@angular/material/button"
+import { MatCardModule } from "@angular/material/card"
+import { MatIconModule } from "@angular/material/icon"
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router"
+
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [MatButtonModule, MatIconModule,RouterOutlet,MatCardModule, MatButtonModule,RouterLinkActive, RouterLink],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [MatButtonModule, MatIconModule, RouterOutlet, MatCardModule, RouterLinkActive, RouterLink],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {
-}
+export class HomeComponent {}
